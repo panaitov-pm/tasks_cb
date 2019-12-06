2d6489a12d295b949cea2cca9d61f2442238e50b
Pavel <p.panaitov@travellizy.com>
2019-12-06 14:00:50 +0200

    Add task list markup

--- src/View/Section/Tasks/TaskList.tsx
+++ src/View/Section/Tasks/TaskList.tsx
@@ -10,29 +10,34 @@ import Task from './Task';
  * @interface Props
  */
 interface Props {
-
+    setIsOpenTaskModal: (isOpen: boolean) => void;
 }
 
-const TaskList: React.FC<Props> = withTasks(({tasks}): any => {
+/**
+ * @type {(props: {readonly tasks?: any}) => any}
+ */
+const TaskList: React.FC<Props> = withTasks(({ tasks, setIsOpenTaskModal }): any => {
     return (
-        <div className="table">
-            <div className="d-flex font-weight-bold table-row">
-                <div className="table-row__column text-center">#</div>
-                <div className="table-row__column text-center" />
-                <div className="table-row__column">Name</div>
-                <div className="table-row__column flex-grow-1 text-nowrap">Description</div>
-                <div className="table-row__column text-center">Delete</div>
+        <>
+            <div className="table">
+                <div className="d-flex font-weight-bold table-row">
+                    <div className="table-row__column text-center">#</div>
+                    <div className="table-row__column text-center" />
+                    <div className="table-row__column">Name</div>
+                    <div className="table-row__column flex-grow-1 text-nowrap">Description</div>
+                    <div className="table-row__column text-center">Delete</div>
+                </div>
+                {
+                    tasks.map((task: ITask, index: number) => (
+                        <Task
+                            key={task.id}
+                            task={task}
+                            index={index + 1}
+                        />
+                    ))
+                }
             </div>
-            {
-                tasks.map((task: ITask, index: number) => (
-                    <Task
-                        key={task.id}
-                        task={task}
-                        index={index + 1}
-                    />
-                ))
-            }
-        </div>
+        </>
     );
 });
 
