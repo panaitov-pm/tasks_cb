4ff8cdedf5704059dbce35ab5a3c712130e077ae
Pavel <p.panaitov@travellizy.com>
2019-12-07 08:28:43 +0200

    Improved hasError method, added test one

--- src/Helper/Error/hasError.ts
+++ src/Helper/Error/hasError.ts
@@ -3,7 +3,11 @@
  * @return {boolean}
  */
 const hasError = <T>(errors: T): boolean => {
-    return Object.values(errors).some(value => value.length === 0)
+    return Object.values(errors).some(value => {
+        if (value === null || value=== undefined) return true;
+
+        return value.length === 0
+    })
 };
 
 export default hasError;
