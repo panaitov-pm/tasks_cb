53368c5c954401bff5549888fdbdffef59589d9f
Pavel <p.panaitov@travellizy.com>
2019-12-08 13:26:22 +0200

    Add animation to add Task

--- src/View/Section/Tasks/TaskList.tsx
+++ src/View/Section/Tasks/TaskList.tsx
@@ -1,10 +1,13 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import CSSTransition from 'react-transition-group/CSSTransition';
+
 
 import '../../../scss/task-list.scss';
 
 import withTasks from '../../../Context/Tasks/withTasks';
 import ITask from '../../../Types/Tasks/Task';
 import Task from './Task';
+import Fade from '../../Modules/Animation/Fade';
 
 /**
  * @interface Props
@@ -17,8 +20,16 @@ interface Props {
  * @type {(props: {readonly tasks?: any}) => any}
  */
 const TaskList: React.FC<Props> = withTasks(({ tasks, setIsOpenTaskModal }): any => {
+
+    const [isFirstRender, setFirstRender] = useState(true);
+
+    useEffect(() => {
+        setFirstRender(false);
+    }, []);
+
     return useMemo(() => (
-        <div className="table">
+        <main className="table">
+            <Fade delay={100} className="fade-up">
             <div className="d-flex font-weight-bold table-row">
                 <div className="table-row__column text-center">#</div>
                 <div className="table-row__column text-center" />
@@ -26,16 +37,18 @@ const TaskList: React.FC<Props> = withTasks(({ tasks, setIsOpenTaskModal }): any
                 <div className="table-row__column flex-grow-1 text-nowrap">Description</div>
                 <div className="table-row__column text-center">Delete</div>
             </div>
+            </Fade>
             {
                 tasks.map((task: ITask, index: number) => (
                     <Task
+                        isFirstRender={isFirstRender}
                         key={task.id}
                         task={task}
                         index={index + 1}
                     />
                 ))
             }
-        </div>
+        </main>
     ), [tasks]);
 });
 
